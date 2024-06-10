7b2a420bd6f51acf66368367f5495ad9ac1bf9bb
samuelecozzubbo <samuele.cozzubbo@gmail.com>
2024-06-10 15:23:13 +0200

    Build: remove static markup and insert all the images dynamically using the provided array and a simple for loop that concatenates a template literal

--- js/script.js
+++ js/script.js
@@ -1,4 +1,24 @@
 //Dato un array contenente una lista di cinque immagini, creare un carosello come nello screenshot allegato.
 
+//array immagini carosello
+const imgArray = ["img/01.webp", "img/02.webp", "img/03.webp", "img/04.webp", "img/05.webp",];
+
+//prendo il container
+const itemsContainer = document.querySelector(".carousel-container");
+
+//Estrapolo elememnti arrayimg
+for (let i = 0; i < imgArray.length; i++) {
+    itemImg = imgArray[i];
+    console.log(itemImg);
+    //creare ad ogni iterazione l'elemento da inserire
+    let itemContent = `<div class="item">
+                            <img src="${itemImg}">
+                            <div class="arrowhead down">&#709;</div>
+                            <div class="arrowhead up">&#708;</div>
+                        </div>`;
+    //inseriamo il nuovo elemento nel contenitore
+    itemsContainer.innerHTML += itemContent;
+
+}
 
 
